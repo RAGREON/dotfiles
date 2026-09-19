65623628f75994d4cf1e4e48d27993ef917370d7
RAGREON <ragreon809@gmail.com>
2026-09-19 16:05:35 +0545

    chore: change smear-cursor config

--- nvim/lua/plugins/ui/smear_cursor.lua
+++ nvim/lua/plugins/ui/smear_cursor.lua
@@ -5,10 +5,10 @@ return {
 		cursor_color = "#d3cdc3",
 
 		stiffness = 0.5,
-		trailing_stiffness = 0.3,
-		damping = 1,
+		trailing_stiffness = 0.7,
+		damping = 0.2,
 
-		distance_stop_animating = 1,
+		distance_stop_animating = 2,
 		never_draw_over_target = false,
 	},
 
